e24333487b27ce7846b8d1f2076eda325f9db7ef
Jawad Almuttawa <almuttawa.jawad@gmail.com>
2025-04-21 15:00:05 +0300

    Added a script to connect to AWS from an ESP32.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,116 @@
-#include <Arduino.h>
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+ 
+#include "DHT.h"
+#define DHTPIN 12     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22
+ 
+#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+ 
+float h ;
+float t;
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+  Serial.println(message);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+ 
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println("Connecting to Wi-Fi");
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+ 
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
+  // Create a message handler
+  client.setCallback(messageHandler);
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(100);
+  }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ 
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["humidity"] = h;
+  doc["temperature"] = t;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+ 
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  connectAWS();
+  dht.begin();
+}
+ 
+void loop()
+{
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+ 
+ 
+  if (isnan(h) || isnan(t) || h > 100.0 || t < -40.0 || t > 80.0)  // Check if any reads failed and exit early (to try again).
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+ 
+  Serial.print(F("Humidity: "));
+  Serial.print(h, 1);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t, 1);
+  Serial.println(F("°C "));
+ 
+  publishMessage();
+  client.loop();
+  delay(2000);
 }
